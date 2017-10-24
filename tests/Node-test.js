b66d398693b6c173a0f81efd59ef6a0d8598c3a3
Matthew Renn <rennmatthewp@gmail.com>
2017-10-23 19:04:38 -0600

    test the test hookups

---
+++ tests/Node-test.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import { Node } from '../lib/Node';
+
+describe('Node', () => {
+  it('should be a function', () => {
+    expect(Node).to.be.a('function');
+  })
+})
